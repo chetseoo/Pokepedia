7e055092cb497d322ffef25754a2d71da6081edb
seoyeon <102604192+choiseoo@users.noreply.github.com>
2023-08-07 22:36:35 +0900

    Initial Commit

---
+++ Pokepedia/PokepediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokepediaApp.swift
+//  Pokepedia
+//
+//  Created by 최서연 on 2023/08/03.
+//
+
+import SwiftUI
+
+@main
+struct PokepediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
